4e255aa4a55ab61a3e0438dd352caf752b2e9d73
arieljsmith <arieljeansmith@gmail.com>
2021-11-19 11:58:48 -0800

    Unhose folders

---
+++ Week 3/horse.py
@@ -0,0 +1,43 @@
+'''For this exercise, you are going to write a program that models the 
+basketball shooting game, HORSE.
+
+First, rename this file to horse.py
+
+- This game has two players.
+- Players alternate taking shots, and, if they miss, they get a letter.
+- For the purposes of this exercise, it will be random whether a player makes
+or misses a given shot.'''
+
+# Hints for getting started
+
+# What variables will you need for this program? In other words, what values
+# do you need to keep track of? 
+
+# What functions will you need? What, if any, input will each function require? 
+# What, if any, output will the function return? What, if any side effects will
+# the function have? For example, will any values be changed?
+
+# For Round 1, DO NOT WRITE ANY CODE. Use comments and psuedocode to plan out
+# what you are going to do. For example, you could do something like:
+# def shoot(player):
+#   determines whether player makes or misses shot
+#   updates players letters if missed
+
+# I will hit up each group individually after round one and get you set for round 2.
+
+# ==================================================================================
+# ==================================================================================
+
+# start variables for player 1 and player 2 that just equal an empty list
+    # player 1 = []
+    # player 2 = []
+# need a prepopulated list that has horse--if they already have that letter, give them the next one
+    # horse = ["h", "o", "r", "s", "e"]
+# while player 1 or player 2 do not have a full "horse", you keep playing the game
+    # while (player 1 or player 2) != horse
+# some kind of random number generator--coinflip thing? if heads, shot made, if tails, miss (SEE TURTLE GAME)
+# if the list fills up with horse, they lose, and other player wins
+
+
+
+
